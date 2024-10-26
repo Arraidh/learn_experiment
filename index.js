4f55dfb260d44c2a0bfbce55c9986d6d1c640743
Kadek Ari Dharmika <ari.darmika@gmail.com>
2024-10-26 17:12:45 +0800

    feat: add movie mode and schema

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
+
+//Structurize the data
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  rating: String,
+});
+
+// Bind the schema to be model database
+const Movie = mongoose.model("Movie", movieSchema);
+
+const amadeus = new Movie({
+  title: "Amadeus",
+  year: 1986,
+  score: 5,
+  rating: "Nice la",
+});
